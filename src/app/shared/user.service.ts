7210a9bba53549bce15f350edc5a0d4ba09ca511
lewisorenge <lewisorenge@gmail.com>
2018-10-03 08:34:03 +0300

    Form validation and send data

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+import { User } from '../user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  readonly rootUrl = '';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  registerUser(user: User){
+    const body: User = {
+      UserName: user.UserName,
+      Password: user.Password,
+      Email: user.Email,
+      FirstName: user.FirstName,
+      Lastname: user.Lastname
+    }
+    return this.http.post(this.rootUrl, body);
+  }
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user.model';
 import { NgForm } from '@angular/forms';
+import { UserService } from '../shared/user.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -10,9 +11,10 @@ import { NgForm } from '@angular/forms';
 export class SignUpComponent implements OnInit {
   user: User;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.resetForm();
   }
 
   resetForm(form?: NgForm){
@@ -24,6 +26,13 @@ export class SignUpComponent implements OnInit {
       this.user.FirstName = '';
       this.user.Lastname = '';
     }
+  }
+
+  OnSubmit(form: NgForm){
+    this.userService.registerUser(form.value)
+      .subscribe( response => {
+        console.log(response);
+      })
   }
 
 }
